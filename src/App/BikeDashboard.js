c438cb123967e887d758587422f133e690dc0444
Kai Dewey <deweykai5@gmail.com>
2020-07-15 18:45:35 +0900

    Move BikeDataTable into own file

--- src/App/BikeDashboard.js
+++ src/App/BikeDashboard.js
@@ -1,43 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import BikeDataTable from './BikeDataTable.js';
 import { useParams } from 'react-router-dom';
 
-const BikeDataTable = ({ bikeData }) => {
-    const tableContents = bikeData.map(createTableRow);
-
-    return (
-        <TableContainer component={Paper}>
-          <Table size='small'>
-            <TableHead>
-              <TableRow>
-                <TableCell>Created At</TableCell>
-                <TableCell align='right'>Voltage</TableCell>
-                <TableCell align='right'>Current</TableCell>
-                <TableCell align='right'>RPM</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {tableContents}
-            </TableBody>
-          </Table>
-        </TableContainer>
-    );
-};
-
-const createTableRow = data => (
-    <TableRow key={data.created_at}>
-      <TableCell align="right">{data.created_at}</TableCell>
-      <TableCell align="right">{data.voltage}</TableCell>
-      <TableCell align="right">{data.current}</TableCell>
-      <TableCell align="right">{data.rpm}</TableCell>
-    </TableRow>
-);
 
 const BikeDashboard = () => {
     let { bike_id } = useParams();
