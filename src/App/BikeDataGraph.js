eb3a67a10e6349e58bbaffde4550aab6d8a1a067
Kai Dewey <deweykai5@gmail.com>
2020-07-15 19:40:33 +0900

    added some styling to graph

--- src/App/BikeDataGraph.js
+++ src/App/BikeDataGraph.js
@@ -2,19 +2,29 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import {
     FlexibleWidthXYPlot as XYPlot,
+    XAxis,
+    YAxis,
+    Crosshair,
+    VerticalGridLines,
+    HorizontalGridLines,
     LineSeries,
 } from 'react-vis';
 
 
 const BikeDataGraph = ({ bikeData }) => {
     const data = bikeData.map((bike, idx) => ({
-        x: bike.created_at,
+        x: bike.created_at * 1000,
         y: bike.current,
     }));
 
     return (
         <Paper>
-          <XYPlot height={300} xType="time">
+          <XYPlot height={300} yDomain={[0, 50]} xType="time">
+            <VerticalGridLines />
+            <HorizontalGridLines />
+            <XAxis />
+            <YAxis />
+
             <LineSeries data={data} />
           </XYPlot>
         </Paper>
