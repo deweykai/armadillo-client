746a37916d003e983d43a482c362168b41b1b59e
Mika Nikura <deweykai5@gmail.com>
2020-08-01 14:46:23 +0900

    remove unused imports

--- src/App/BikeDashboard.js
+++ src/App/BikeDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import BikeDataIndicator from './features/bikeData/BikeDataIndicator';
 import BikeDescription from './features/orgData/BikeDescription';
 import BikePowerGraph from './features/bikeData/BikePowerGraph';
