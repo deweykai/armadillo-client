e359aaedfb1aea88904564540d16fc59b4bf908c
Mika Nikura <deweykai5@gmail.com>
2020-08-03 23:08:00 +0900

    add graph hints

--- src/App/components/PowerGraph.js
+++ src/App/components/PowerGraph.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import {
@@ -9,29 +9,52 @@ import {
     HorizontalGridLines,
     AreaSeries,
     LineSeries,
+    Hint,
 } from 'react-vis';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import useStyles from '../styles';
 import PropTypes from 'prop-types';
 import { useTheme } from '@material-ui/core/styles';
 
+
 const PowerGraph = ({data}) => {
     const classes = useStyles();
     const theme = useTheme();
+
+    const [value, setValue] = useState(false);
+
+    const onNearestX = (value, info)=> {
+        setValue(value);
+    };
+
+    const onMouseLeave = () => {
+        setValue(false);
+    };
 
     return (
         <Paper className={classes.graphPaper}>
             <Typography variant="h5">Power</Typography>
             <AutoSizer disableHeight>
                 {({width}) => (
-                    <XYPlot height={250} width={width} yDomain={[0, 1500]} xType="time">
+                    <XYPlot onMouseLeave={onMouseLeave} height={250} width={width} yDomain={[0, 1500]} xType="time">
                         <XAxis />
                         <YAxis />
                         <VerticalGridLines />
                         <HorizontalGridLines />
 
-                        <AreaSeries data={data} curve={'curveMonotoneX'} opacity={0.6} color={theme.palette.secondary.light} />
-                        <LineSeries data={data} curve={'curveMonotoneX'} color={theme.palette.secondary.main} />
+                        <AreaSeries
+                            data={data}
+                            curve={'curveMonotoneX'}
+                            color={theme.palette.secondary.light}
+                            opacity={0.6}
+                        />
+                        <LineSeries
+                            data={data}
+                            curve={'curveMonotoneX'}
+                            color={theme.palette.secondary.main}
+                            onNearestX={onNearestX}
+                        />
+                        {value ? <Hint value={value} /> : null}
                     </XYPlot>
                 )}
             </AutoSizer>
