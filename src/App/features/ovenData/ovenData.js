730fc47da2a50bf06cb02eb5f8fa14ee3a2c4861
Mika Nikura <deweykai5@gmail.com>
2020-08-05 23:43:05 +0900

    add input type assertion

--- src/App/features/ovenData/ovenData.js
+++ src/App/features/ovenData/ovenData.js
@@ -2,6 +2,7 @@ import {ovenDataSelector} from '../sourceData/sourceDataSlice';
 import {useSelector} from 'react-redux';
 
 export const useOvenData = (ovenId) => {
+    console.assert(typeof ovenId === "number");
     let ovenData = useSelector(ovenDataSelector(ovenId));
     return ovenData;
 };
