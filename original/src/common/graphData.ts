4fba607fcb92bf93fbf0f2612b29ebdfecd67252
Mika Nikura <deweykai5@gmail.com>
2020-09-02 23:57:59 +0900

    finalize directory changes

--- original/src/common/graphData.ts
+++
@@ -1,6 +0,0 @@
-interface GraphData {
-    x: number,
-    y: number,
-};
-
-export type {GraphData};
--- original/src/components/PowerGraph.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import Graph from './Graph';
-import type {GraphData} from '../common/graphData';
-
-const PowerGraph = ({data, title, missingMsg} : {data: GraphData[] | null, title: string, missingMsg: string}) => (
-    <Graph data={data} title={title} missingMsg={missingMsg} />
-);
-
-export default PowerGraph;
